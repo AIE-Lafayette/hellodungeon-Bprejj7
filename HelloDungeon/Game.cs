69b45c83f47340be7ddbe6ede8d81f16aa1ae4ab
Braxton Prejean <bprejj7@gmail.com>
2023-08-23 15:05:54 -0500

    Added player stats and completed operator exercise

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; 
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +9,26 @@ namespace HelloDungeon
     class Game
     {
         public void Run()
-        {
+        {   // Initliazes player stats.
+            string playerName = "Kuhla";
+            float damage = 3.2f;
+            float health = 100.0f * 2;
+            health++;
+            int areaNumber = 0;
+            bool playerIsAlive = true;
+
+            //Print out name for first excersize.
+            Console.WriteLine("Kane");
+            Console.WriteLine("Prejean");
+
+            //Get player input for first and last character name.
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            playerName = firstName + lastName;
+
+            // Display character name for player feedback.
+            Console.WriteLine("Hello " + playerName);
+            Console.WriteLine("Welcome to Wonder City");
 
         }
     }
